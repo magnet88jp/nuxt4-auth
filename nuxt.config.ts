94147e8aa41a69f12a0da3a21fd46c9861e213d7
Shinsaku Nishino <magnet@MagsBookPro.local>
2025-10-27 12:06:26 +0900

    fix style

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
   modules: ['@nuxt/eslint', '@nuxt/test-utils', '@nuxt/ui'],
+  devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
+  compatibilityDate: '2025-07-15',
   eslint: {
     config: {
       stylistic: {
