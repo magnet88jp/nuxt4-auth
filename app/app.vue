69961078c8b4198e96105c52c5be3c2869433601
Shinsaku Nishino <magnet@MagsBookPro.local>
2025-10-27 11:57:45 +0900

    add eslint test code

--- app/app.vue
+++ app/app.vue
@@ -2,6 +2,9 @@
   <div>
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
+    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+  Button
+</button>
   </div>
 </template>
 
